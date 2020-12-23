91c661c0626a7a16c4c98ca3bce84781ae16191c
Ndumiso Onke Fanti <0nk3.0nk3@gmail.com>
2020-12-23 19:42:59 +0200

    started react app

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+// import './App.css';, { Component }
+
+function App() {
+  return (
+    <div className="app">
+      <div className="header">Giving Needs</div>
+      <div className="slogan">Giving back is a tap away</div>
+    </div>
+  )
+}
+
+
+export default App;
+//npm start
